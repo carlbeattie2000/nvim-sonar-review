2d7876de6822f6d148a60415026658e21de9a852
Carl Beattie <12beattiecastp@gmail.com>
2025-03-08 16:12:01 +0000

    feat: keybinds

--- plugin/sonar-review.lua
+++ plugin/sonar-review.lua
@@ -2,3 +2,6 @@ if vim.g.loaded_sonar_review then return end
 vim.g.loaded_sonar_review = 1
 
 local sonar = require("sonar-review")
+
+vim.keymap.set("n", "<leader>br", sonar.show_buffer_reports, { desc = "Buffer Reports" })
+vim.keymap.set("n", "<leader>cr", sonar.show_commit_reports, { desc = "Commit Reports" })
