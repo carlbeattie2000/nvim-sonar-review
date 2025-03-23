0c671b041c980ee2fd7018fdfb835e5cc8b01eb2
= <12beattiecastp@gmail.com>
2025-03-23 15:38:18 +0000

    fix: Freezes due to curl timeout
    This is a temp fixed, and will be replaced with async callbacks

--- lua/sonar-review/sonar_api.lua
+++ lua/sonar-review/sonar_api.lua
@@ -9,13 +9,20 @@ function M.get_hotspots(query)
   local env, _ = utils.load_env()
   local token = env.SONAR_TOKEN or "admin"
   local sonar_address = os.getenv("SONAR_ADDRESS") or "http://localhost:9000"
+
+  if not env or not token or not sonar_address then
+    vim.notify("Could not get required enviroment variables.", vim.log.levels.INFO)
+    return {}
+  end
+
   local hotspot_query = query:gsub("componentKeys=", "project=")
-  local cmd = string.format("curl -s -u %s: '%s/api/hotspots/search?%s'", token, sonar_address, hotspot_query)
+  local cmd = string.format("curl -s --max-time 2 -u %s: '%s/api/hotspots/search?%s'", token, sonar_address,
+    hotspot_query)
   local result = vim.fn.system({ "sh", "-c", cmd })
   local ok, decoded = pcall(vim.fn.json_decode, result)
 
-  if decoded.errors then
-    return {}
+  if decoded.errors or not ok then
+    return nil
   end
 
   return ok and decoded.hotspots or {}
@@ -27,10 +34,19 @@ function M.get_issues(query)
   local env = utils.load_env()
   local token = env.SONAR_TOKEN or "admin"
   local sonar_address = os.getenv("SONAR_ADDRESS") or "http://localhost:9000"
-  local cmd = string.format('curl -s -u %s: "%s/api/issues/search?%s"', token, sonar_address, query)
-  vim.print(cmd)
+
+  if not env or not token or not sonar_address then
+    vim.notify("Could not get required enviroment variables.", vim.log.levels.INFO)
+    return {}
+  end
+
+  local cmd = string.format('curl -s --max-time 2 -u %s: "%s/api/issues/search?%s"', token, sonar_address, query)
   local result = vim.fn.system({ "sh", "-c", cmd })
   local ok, decoded = pcall(vim.fn.json_decode, result)
+
+  if not ok then
+    return nil
+  end
 
   return ok and decoded.issues or {}
 end
@@ -38,6 +54,11 @@ end
 function M.get_issues_and_hotspots(query)
   local config = sonarReview.get_config()
   local issues = M.get_issues(query)
+
+  if not issues then
+    vim.notify("Issue fetching results from sonar server.", vim.log.levels.ERROR)
+    return {}
+  end
 
   if not config.include_security_hotspots_insecure then
     return issues
@@ -47,7 +68,10 @@ function M.get_issues_and_hotspots(query)
   local combined_issues = {}
 
   for _, issue in ipairs(issues) do table.insert(combined_issues, issue) end
-  for _, issue in ipairs(hotspots) do table.insert(combined_issues, issue) end
+
+  if hotspots then
+    for _, issue in ipairs(hotspots) do table.insert(combined_issues, issue) end
+  end
 
   return combined_issues
 end
